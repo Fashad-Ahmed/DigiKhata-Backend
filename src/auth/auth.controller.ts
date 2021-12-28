0ae7d80c07f06f6bd1efa89938e8957e81a84a2e
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2021-12-28 18:34:43 +0500

    In API created | initiated

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,6 +6,21 @@ import { Request } from 'express';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Post('/sigin')
+  async signin(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Body('FCMtoken') FCMtoken: string,
+  ) {
+    try {
+      const res = await this.authService.signin(email, password, FCMtoken);
+      console.log(`signed in`);
+      return { result: res };
+    } catch (error) {
+      return error;
+    }
+  }
+
   @Post('/signup')
   async signup(@Req() request: Request) {
     try {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Auth } from './auth.model';
@@ -9,9 +9,30 @@ export class AuthService {
   products: Auth[] = [];
   constructor(@InjectModel('Auth') private readonly authModel: Model<Auth>) {}
 
-  //   async signin(email, password, FCMtoken) {
+  async signin(email, password, FCMtoken) {
+    console.log('sign in request started');
+    console.log('signin body', email, password, FCMtoken);
 
-  //   }
+    try {
+      const userExist = await this.authModel.findOne({ email: email });
+      if (!userExist) {
+        throw new NotFoundException('User does not exist');
+      }
+
+      if (password != userExist.password) {
+        console.log('Invalid credentials');
+        throw [404];
+      }
+
+      const user = {
+        userExist,
+      };
+      return { ...user, statusCode: 200 };
+    } catch (error) {
+      console.log(error);
+      throw [404, error.message];
+    }
+  }
 
   async signup(req) {
     console.log('sign up request started');
